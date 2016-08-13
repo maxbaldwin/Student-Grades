d3230268c68a917f9c756726f3a9099ea6738ba9
Max Baldwin <maxrbaldwin@gmail.com>
2016-08-13 09:48:27 -0500

    solved grade range question using an else if statement

--- grades.js
+++ grades.js
@@ -1,13 +1,39 @@
 var gradeSchema = {
-  gradeA = 0; 
-  gradeB = 0; 
-  gradeC = 0; 
-  gradeD = 0; 
-  gradeF = 0; 
+  gradeA: 0,
+  gradeB: 0, 
+  gradeC: 0, 
+  gradeD: 0, 
+  gradeF: 0, 
 }
 
+//given in instructions
 var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
 
+//for loop for how many letter grades each are
+for (var i = 0; i < scores.length; i++) {
+  if (scores[i] < 61) {
+    gradeSchema.gradeF++; 
+  }
+  else if (scores[i] < 71) {
+    gradeSchema.gradeD++;
+  }
+  else if (scores[i] < 81) {
+    gradeSchema.gradeC++;
+  }
+  else if (scores[i] < 91) {
+    gradeSchema.gradeB++; 
+  }
+  else if(scores[i] < 100) {
+    gradeSchema.gradeA++;
+  }
+}
+
+//console logs of grades in the scores variable that are in the letter grade range
+console.log(gradeSchema.gradeA); 
+console.log(gradeSchema.gradeB); 
+console.log(gradeSchema.gradeC); 
+console.log(gradeSchema.gradeD); 
+console.log(gradeSchema.gradeF); 
 
 
 //How many of each grade?
