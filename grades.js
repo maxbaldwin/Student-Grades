172a9f9e67a49b380eb38bb9d8fd067640efe793
Max Baldwin <maxrbaldwin@gmail.com>
2016-08-13 09:32:12 -0500

    setting up loop question at beginning

--- grades.js
+++ grades.js
@@ -1,4 +1,14 @@
+var gradeSchema = {
+  gradeA = 0; 
+  gradeB = 0; 
+  gradeC = 0; 
+  gradeD = 0; 
+  gradeF = 0; 
+}
+
 var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+
+
 
 //How many of each grade?
 var count = 0;
@@ -15,3 +25,4 @@ console.log(minMath);
 //What is the highest grade?
 var maxMath = Math.max(...scores); 
 console.log(maxMath); 
+
