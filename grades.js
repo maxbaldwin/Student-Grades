b87f1c9d6296770f8d8cc918d3185c36bb1f13be
Max Baldwin <maxrbaldwin@gmail.com>
2016-08-13 09:13:38 -0500

    solved 1 out of 3

---
+++ grades.js
@@ -0,0 +1,13 @@
+var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+
+//How many of each grade?
+var count = 0;
+for(var i = 0; i < scores.length; ++i){
+    if(scores[i] == 1)
+        count++;
+}
+console.log(scores.length);
+
+//What is the lowest grade?
+
+//What is the highest grade?
